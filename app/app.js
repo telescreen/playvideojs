b2def25fe948753c6f58d019b5a0c33db2d5de4e
Bui Hong Ha <habuihong@gmail.com>
2017-07-02 23:05:06 +0900

    Include playbackrate and video status

--- app/app.js
+++ app/app.js
@@ -1,14 +1,49 @@
 (function() {
-    let video1 = videojs("video1");
-    let video2 = videojs("video2");
+    let videos = [videojs("pnet_video"), videojs("exc_video")];
 
-    $("#play").click(function() {
-        video1.play();
-        video2.play();
+    // Controls
+    var handle = $('#custom-handle');
+    $('#playback_rate').slider({
+        orientation: "horizontal",
+        range: "min",
+        step: 0.25,
+        max: 3,
+        value: 1,
+        create: function() {
+            handle.text( $( this ).slider( "value" ) );
+        },
+        slide: function( event, ui ) {
+            handle.text( ui.value );
+            videos.forEach(function(video) {
+                video.playbackRate(ui.value);
+            });
+        }
     });
 
-    $("#stop").click(function() {
-        video1.pause();
-        video2.pause();
+    videos.forEach(function(video) {
+        video.on('stalled', function(event) {
+            $('#' + this.id() + '_status').text('stalled');
+        });
+        video.on('playing', function(event) {
+            $('#' + this.id() + '_status').text('playing');
+        });
+        video.on('pause', function(event) {
+            $('#' + this.id() + '_status').text('pause');
+        });
+        video.on('canplay', function(event) {
+            $('#' + this.id() + '_status').text('ready');
+        });
+    })
+
+    $("#play").click(function() {
+        videos.forEach(function(video) {
+            video.play();
+        });
+    });
+
+    $("#pause").click(function() {
+        videos.forEach(function(video) {
+            video.pause();
+        });
     });
 })();
